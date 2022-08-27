7a6cc240e81e3105b159dbb331450412d0133984
longtoeboy <tsraikage@gmail.com>
2022-08-27 03:31:54 -0400

    initial upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# nisjustaletter_autolike
+to use this script you'll need python module `Tweepy` also it's necessary to use your keys in `config.py`
+
+tags can be added and removed from `tags.txt`
+
+once everything is correct just start the program using:
+```
+python3 app.py
+```
---
+++ app.py
@@ -0,0 +1,38 @@
+import tweepy
+import config
+import time
+
+def main():
+    try:
+        client=tweepy.Client(
+            config.BEARER_TOKEN,
+            config.API_KEY,
+            config.API_SECRET,
+            config.ACCESS_TOKEN,
+            config.ACCESS_TOKEN_SECRET,
+            wait_on_rate_limit=True
+        )
+        with open("tags.txt","r") as f:
+            tags=[x.strip() for x in f]
+    except Exception as e:
+        print(e)
+        exit()
+    
+    myId=client.get_me().data.id
+    likedTweets=[liked.id for liked in client.get_liked_tweets(myId).data]
+    print("Started autoliker for Nisjustaletter")
+    print("To turn off program press Ctrl + Z and Ctrl + C")
+    while(True):
+        for tag in tags:
+            tweets=client.search_recent_tweets(tag).data
+            for tweet in tweets:
+                if tweet.id in likedTweets:
+                    continue
+                client.like(tweet)
+                likedTweets.append(tweet.id)
+                print(f"tweet id:{tweet.id} liked")
+                time.sleep(15*60/50)
+
+
+if __name__ == "__main__":
+    main()
